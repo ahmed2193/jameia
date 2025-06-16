02fdaf9ea136b109254d37da7838bac20296004e
ahmed2193 <ahmed.alfawali@gmail.com>
2025-06-16 14:02:17 +0300

    first commit

--- lib/Jameia_app.dart
+++ lib/Jameia_app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:jameia/core/config/routing/app_router.dart';
 import 'package:jameia/core/config/theme/app_theme.dart';
+import 'package:jameia/core/constants/app_strings.dart';
 import 'package:jameia/core/di/service_locator.dart' as di;
 import 'package:jameia/core/presentation/cubits/theme_cubit/theme_cubit.dart';
 import 'package:jameia/features/recipes/presentation/blocs/recipes/recipes_bloc.dart';
@@ -23,7 +24,7 @@ class JameiaApp extends StatelessWidget {
       child: BlocBuilder<ThemeCubit, ThemeState>(
         builder: (context, state) {
           return MaterialApp(
-            title: 'jameia',
+            title: AppStrings.appTitle,
             theme: AppTheme.getTheme(context, Brightness.light),
             darkTheme: AppTheme.getTheme(context, Brightness.dark),
             themeMode: state.themeMode,
